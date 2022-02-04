c96487a5c5f58a886b845d443e1890465159efd4
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-02-04 17:47:28 -0500

    added advanced styling to modals

--- frontend/src/context/Modal.js
+++ frontend/src/context/Modal.js
@@ -23,12 +23,33 @@ export function ModalProvider({ children }) {
 
 export function Modal({ onClose, children }) {
 	const modalNode = useContext(ModalContext);
+
+	useEffect(() => {
+		const modalBackground = document.querySelector('#modal-background');
+		const modalContent = document.querySelector('#modal-content');
+		const timeout = () => setTimeout(onClose, 150);
+
+		modalBackground.addEventListener('click', (e) => {
+			if (e.target === modalBackground) {
+				modalContent.classList.remove('fade-in');
+				modalContent.classList.add('fade-out');
+				timeout();
+			}
+
+			return () => {
+				clearTimeout(timeout);
+			};
+		});
+	}, [modalNode]);
+
 	if (!modalNode) return null;
 
 	return ReactDOM.createPortal(
 		<div id='modal'>
-			<div id='modal-background' onClick={onClose} />
-			<div id='modal-content'>{children}</div>
+			<div id='modal-background' />
+			<div id='modal-content' className='fade-in'>
+				{children}
+			</div>
 		</div>,
 		modalNode
 	);
