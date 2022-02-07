9d1879aa3e76b528319aba53618e16a69b00df48
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-02-07 12:28:13 -0500

    get notes and post new note working

--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -1,20 +1,36 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Sidebar.css';
 import { useShowHide } from '../../context/ShowHide';
+import { addNote, getNotes } from '../../store/notes';
 
 function Sidebar({ isLoaded }) {
-	const sessionUser = useSelector((state) => state.session.user);
-	const { expandNote } = useShowHide();
+	const user = useSelector((state) => state.session.user);
+	const { expandNote, setActiveNote } = useShowHide();
+	const dispatch = useDispatch();
 
-	const profileButton = sessionUser ? <ProfileButton user={sessionUser} /> : null;
+	const addNewNote = async (e) => {
+		e.preventDefault();
+
+		const newNote = await dispatch(addNote(user.id));
+		dispatch(getNotes(user?.id));
+		setActiveNote(newNote.id);
+	};
+
+	const profileButton = user ? <ProfileButton user={user} /> : null;
 
 	return (
 		!expandNote && (
 			<div className='sidebar-container slide-from-offscreen'>
 				<ul className='sidebar'>
 					<li>{isLoaded && profileButton}</li>
+					<div id='sidebar-menu-items'>
+						<button id='add-note-button' onClick={addNewNote}>
+							<i className='fas fa-plus' style={{ paddingRight: '10px' }}></i>
+							Add Note
+						</button>
+					</div>
 				</ul>
 			</div>
 		)
