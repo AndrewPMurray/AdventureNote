649b3a10d790e23b32777af58a92a58a1403c61f
AndrewPMurray <andrewpmurray1987@gmail.com>
2022-02-10 13:38:26 -0500

    added backend error handling for notebook titles and refactored handling for closing modals when form successfully submits

--- frontend/src/components/DeleteNotebookModal/DeleteNotebook.js
+++ frontend/src/components/DeleteNotebookModal/DeleteNotebook.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-import { deleteNotebook, getNotebooks } from '../../store/notebooks';
+import { deleteNotebook } from '../../store/notebooks';
 import './DeleteNotebook.css';
 
 const DeleteNotebook = ({ setShowModal, notebook }) => {
 	const dispatch = useDispatch();
-	const user = useSelector((state) => state.session.user);
 	const [disabled, setDisabled] = useState(true);
 
 	useEffect(() => {
